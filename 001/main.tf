fda498fe20b8fc6b08e12e9087dd2f968b42a895
VitaliiStorozh <sv301185@gmail.com>
2021-06-08 12:46:22 +0300

    Change tags for instances

--- 001/main.tf
+++ 001/main.tf
@@ -1,3 +1,10 @@
+#---------------------------------------------------------
+# My terraform_remote_state
+#
+# Build WebServerduring bootstrap_action
+#
+# Made by Vitalii_Storozh
+
 provider "aws" {
   region = "us-east-2"
 }
@@ -5,4 +12,22 @@ provider "aws" {
 resource "aws_instance" "my_ubuntu" {
   ami           = "ami-00399ec92321828f5"
   instance_type = "t2.micro"
+  count         = 2
+
+  tags = {
+    Name    = "MyUbuntuServer"
+    Owner   = "VitaliiStorozh"
+    Project = "TerraformStudy"
+  }
+}
+
+resource "aws_instance" "my_amazon_linux" {
+  ami           = "ami-077e31c4939f6a2f3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name    = "MyAmazonServer"
+    Owner   = "VitaliiStorozh"
+    Project = "TerraformStudy"
+  }
 }
