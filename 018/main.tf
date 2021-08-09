abd891917ff539eaf85ae9cefb32efac385d4ca6
VitaliiStorozh <sv301185@gmail.com>
2021-08-09 13:49:20 +0300

    Create resources in different regions

--- 018/main.tf
+++ 018/main.tf
@@ -9,3 +9,23 @@
 provider "aws" {
   region = "us-east-2"
 }
+
+resource "aws_iam_user" "user1" {
+  name = "Shevchenko"
+}
+
+resource "aws_iam_user" "users" {
+  count = length(var.aws_users)
+  name  = element(var.aws_users, count.index)
+}
+
+#----------------------------------------------
+
+resource "aws_instance" "servers" {
+  count         = 3
+  ami           = "ami-00399ec92321828f5"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "Server Number ${count.index + 1}"
+  }
+}
